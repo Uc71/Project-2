b8e92d2c949b99d4cb2a4d4e334774b371754095
KenSaville <45977942+KenSaville@users.noreply.github.com>
2021-03-30 22:59:34 -0400

    Update logic.js
    was able to get crime markers added to the map with popups showing type of crime.  There is an error message about not being able to find lat for a null.  So maybe there is some missing data.
    
    Haven't added street lights yet.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,37 +1,78 @@
 // Creating our initial map object
 // We set the longitude, latitude, and the starting zoom level
 // This gets inserted into the div with an id of 'map'
-var myMap = L.map("map", {
+//var myMap = L.map("map", {
   
-  center: [35.791473, -78.78114],
-  zoom: 13
-});
+ // center: [35.791473, -78.78114],
+ // zoom: 13
+//});
 
 // Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
 
-var url = "https://data.townofcary.org/api/v2/catalog/datasets/cpd-incidents/exports/geojson"
+function createMap(crimeReports) {
 
-//console.log(url)
+    // Create the tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+  
+    // Create a baseMaps object to hold the lightmap layer
+    var baseMaps = {
+      "Light Map": lightmap
+    };
+  
+    // Create an overlayMaps object to hold the bikeStations layer
+    var overlayMaps = {
+      "Crime Reports": crimeReports
+    };
+  
+    // Create the map object with options
+    var map = L.map("map", {
+      center: [35.791473, -78.78114],
+      zoom: 12,
+      layers: [lightmap, crimeReports]
+    });
+  
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+  }
 
-d3.json(url, function(error, data){
-  console.log(data)
-})
+function createMarkers(response) {
 
-// read response body and parse as JSON
+  // Pull the "stations" property off of response.data
+  //var = response.data.stations;
 
-// Loop through the streetlights array and create one marker for each light, bind a popup containing its name and population add it to the map
-//for (var i = 0; i < cities.length; i++) {
-//  var city = cities[i];
-//  L.marker(city.location)
-//    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
-//    .addTo(myMap);
-//}
+  // Initialize an array to hold bike markers
+  var crimeMarkers = [];
+  var crimes_2020 = [];
+  //var crime_2019 =[]:
+
+  // Loop through the incident array, filter for years 2019 and 2020
+  for (var index = 0; index < response.length; index++) {
+    if (response[index].year === "2020") {
+      crimes_2020.push(response[index]);
+      crime_20 = response[index]
+  //else if (response[index].year === "2019"){crime_2019.push(response[index])};
+    
+  // For each crime, create a marker and bind a popup with the crime's name
+      var crimeMarker_2020 = L.marker([crime_20.lat, crime_20.lon])
+      .bindPopup("<h3>" + "Crime = " + crime_20.crime_type + "</h3>");
+
+    // Add the marker to the crimeMarkers array
+      crimeMarkers.push(crimeMarker_2020);
+  }
+  }
+  // Create a layer group made from the bike markers array, pass it into the createMap function
+  createMap(L.layerGroup(crimeMarkers));
+
+  }
+
+//var url = "https://data.townofcary.org/api/v2/catalog/datasets/cpd-incidents/exports/geojson"
+var url = "https:data.townofcary.org/api/v2/catalog/datasets/cpd-incidents/exports/json"
+
+d3.json(url, createMarkers);
