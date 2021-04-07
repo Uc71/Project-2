a45f72887f727bda19c99c19ad83f9e0aa543cf2
scottthomas586 <73846307+scottthomas586@users.noreply.github.com>
2021-04-07 19:48:21 -0400

    Add files via upload

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoic2NvdGhvbTI4IiwiYSI6ImNrbWh0ODE2NjBhaXcydW52bDAxc3ZjanMifQ.B75doxXbBUBJmEI1jbkomQ";
+const API_KEY = "pk.eyJ1Ijoic2NvdGhvbTI4IiwiYSI6ImNrbXpnbmJyaDBkcDkydm1meXV1em1vOHUifQ.uQago-sbmkEIsns1XSBLFA";
